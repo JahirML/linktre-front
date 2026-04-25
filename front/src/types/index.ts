c98c174fceb36e73bf69a44dbf183ea23d5dde3b
Jahir Mora <jahiragustin1234@gmail.com>
2026-04-24 21:36:40 -0600

    add login form type

--- front/src/types/index.ts
+++ front/src/types/index.ts
@@ -8,3 +8,7 @@ export type RegisterForm = Pick<User, "email" | "name" | "handle"> & {
   password: string;
   repeatPassword: string;
 };
+
+export type LoginForm = Pick<User, "email"> & {
+  password: string;
+};

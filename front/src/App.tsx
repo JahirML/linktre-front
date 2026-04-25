3367de793a5c53e98b1b09f948ace57b3551ca47
Jahir Mora <jahiragustin1234@gmail.com>
2026-04-24 21:50:46 -0600

    implement query client

--- front/src/App.tsx
+++ front/src/App.tsx
@@ -3,17 +3,26 @@ import "./App.css";
 import Login from "./pages/Login";
 import Register from "./pages/Register";
 import AuthLayout from "./components/Layouts/AuthLayout";
+import { Toaster } from "react-hot-toast";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+
+const queryClient = new QueryClient();
 
 function App() {
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route element={<AuthLayout />}>
-          <Route path="/auth/login" element={<Login />} />
-          <Route path="/auth/register" element={<Register />} />
-        </Route>
-      </Routes>
-    </BrowserRouter>
+    <>
+      <QueryClientProvider client={queryClient}>
+        <BrowserRouter>
+          <Routes>
+            <Route element={<AuthLayout />}>
+              <Route path="/auth/login" element={<Login />} />
+              <Route path="/auth/register" element={<Register />} />
+            </Route>
+          </Routes>
+        </BrowserRouter>
+      </QueryClientProvider>
+      <Toaster position="top-center" />
+    </>
   );
 }
 

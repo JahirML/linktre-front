c000e821878f86c8289d49820b102225294b61e8
Jahir Mora <jahiragustin1234@gmail.com>
2026-04-24 22:07:37 -0600

    implement login user api

--- front/src/api/userApi.ts
+++ front/src/api/userApi.ts
@@ -1,11 +1,22 @@
 import api from "../config/axios";
-import type { RegisterForm } from "../types";
+import type { LoginForm, RegisterForm } from "../types";
 import { isAxiosError } from "axios";
 
 export const registerUser = async (formData: RegisterForm) => {
   try {
     const { data } = await api.post("/auth/register", formData);
-    console.log(data);
+    return data;
+  } catch (error) {
+    if (isAxiosError(error) && error.response) {
+      throw new Error(error.response.data.error);
+    }
+    throw error;
+  }
+};
+
+export const loginUser = async (formData: LoginForm) => {
+  try {
+    const { data } = await api.post("auth/login", formData);
     return data;
   } catch (error) {
     if (isAxiosError(error) && error.response) {

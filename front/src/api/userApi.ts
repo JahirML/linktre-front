f4c9816864d2f673f355f5de868c04dd47cb079b
Jahir Mora <jahiragustin1234@gmail.com>
2026-05-05 20:48:14 -0600

    create get user function

--- front/src/api/userApi.ts
+++ front/src/api/userApi.ts
@@ -1,5 +1,5 @@
 import api from "../config/axios";
-import type { LoginForm, RegisterForm } from "../types";
+import { User, type LoginForm, type RegisterForm } from "../types";
 import { isAxiosError } from "axios";
 
 export const registerUser = async (formData: RegisterForm) => {
@@ -17,6 +17,19 @@ export const registerUser = async (formData: RegisterForm) => {
 export const loginUser = async (formData: LoginForm) => {
   try {
     const { data } = await api.post("auth/login", formData);
+    return data;
+  } catch (error) {
+    if (isAxiosError(error) && error.response) {
+      throw new Error(error.response.data.error);
+    }
+    throw error;
+  }
+};
+
+export const getUser = async () => {
+  try {
+    const { data } = await api.get<User>("/user");
+    console.log(data);
     return data;
   } catch (error) {
     if (isAxiosError(error) && error.response) {

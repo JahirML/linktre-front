6eb4ccc4ee0ec09c8a84101b303ca911c4968ebe
Jahir Mora <jahiragustin1234@gmail.com>
2026-05-19 16:20:44 -0600

    FEAT: add update profile function

--- front/src/api/userApi.ts
+++ front/src/api/userApi.ts
@@ -1,5 +1,10 @@
 import api from "../config/axios";
-import { type User, type LoginForm, type RegisterForm } from "../types";
+import {
+  type User,
+  type LoginForm,
+  type RegisterForm,
+  type UpdateProfileForm,
+} from "../types";
 import { isAxiosError } from "axios";
 
 export const registerUser = async (formData: RegisterForm) => {
@@ -29,6 +34,18 @@ export const loginUser = async (formData: LoginForm) => {
 export const getUser = async () => {
   try {
     const { data } = await api.get<User>("/user");
+    return data;
+  } catch (error) {
+    if (isAxiosError(error) && error.response) {
+      throw new Error(error.response.data.error);
+    }
+    throw error;
+  }
+};
+
+export const updateProfile = async (formData: UpdateProfileForm) => {
+  try {
+    const { data } = await api.patch<User>("/user", formData);
     return data;
   } catch (error) {
     if (isAxiosError(error) && error.response) {

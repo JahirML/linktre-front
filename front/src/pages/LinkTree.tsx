700956453a8db6272f8f74664e0a9a71e26a84df
Jahir Mora <jahiragustin1234@gmail.com>
2026-06-01 19:44:02 -0600

    FEAT: implement social network input links

--- front/src/pages/LinkTree.tsx
+++ front/src/pages/LinkTree.tsx
@@ -1,5 +1,31 @@
+import { useState } from "react";
+import { social } from "../data/social";
+import DevTreeInput from "../components/DevTreeInput";
+
 function LinkTree() {
-  return <div>linktre</div>;
+  const [socialLinks, setSocialLinks] = useState(social);
+
+  const handleUrlChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const { name, value } = e.target;
+    // console.log(name, value);
+    const updatedLinks = socialLinks.map((link) =>
+      link.name === name ? { ...link, url: value } : link,
+    );
+    setSocialLinks(updatedLinks);
+  };
+  return (
+    <>
+      <div className="space-y-5">
+        {socialLinks.map((link) => (
+          <DevTreeInput
+            key={link.name}
+            link={link}
+            handleUrlChange={handleUrlChange}
+          />
+        ))}
+      </div>
+    </>
+  );
 }
 
 export default LinkTree;

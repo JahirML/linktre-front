c5bff0841b530ee2216a2615a9be4596f34945a3
Jahir Mora <jahiragustin1234@gmail.com>
2026-06-01 19:43:10 -0600

    ADD type for social netwrork

--- front/src/types/index.ts
+++ front/src/types/index.ts
@@ -17,3 +17,12 @@ export type LoginForm = Pick<User, "email"> & {
 };
 
 export type UpdateProfileForm = Pick<User, "handle" | "description">;
+
+export type SocialNetwork = {
+  id: number;
+  name: string;
+  url: string;
+  enabled: boolean;
+};
+
+export type DevTreeLink = Pick<SocialNetwork, "enabled" | "name" | "url">;
